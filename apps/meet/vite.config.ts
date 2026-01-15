352891264d812320ee32ba4922c7efc0dd2479d8
Lain <134944634+Lain4504@users.noreply.github.com>
2026-01-15 12:50:20 +0700

    update rewrite url config

--- apps/meet/vite.config.ts
+++ apps/meet/vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5180,
+    proxy: {
+      '/api': {
+        target: 'http://35.239.151.115:8080',
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': '/src',
