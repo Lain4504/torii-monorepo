27f3698c8afa15245968b53eb3c04d77e5f3adf3
finallbossl <nguyenluan.28122003@gmail.com>
2026-01-25 11:09:41 +0700

    chore: update gateway and api client ports to 8080

--- apps/server/modules/gateway/src/main.ts
+++ apps/server/modules/gateway/src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap() {
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new GlobalExceptionsFilter());
 
-  await app.listen(process.env.port ?? 8050);
+  await app.listen(process.env.port ?? 8080);
 }
 bootstrap();
 
