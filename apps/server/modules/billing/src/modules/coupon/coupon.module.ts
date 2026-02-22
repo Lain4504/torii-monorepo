d2bef48be1663af748d8e7cb2bf583b153da10cf
thanhphuongne <lephuong14052004@gmail.com>
2026-02-22 20:30:44 +0700

    feat: auto mapper profile for billing service

--- apps/server/modules/billing/src/modules/coupon/coupon.module.ts
+++ apps/server/modules/billing/src/modules/coupon/coupon.module.ts
@@ -3,11 +3,12 @@ import { CouponController } from './coupon.controller';
 import { CouponService } from './coupon.service';
 import { CouponRepository } from './coupon.repository';
 import { RedisModule } from '@server/shared';
+import { CouponProfile } from '@server/billing/infrastructure/mappings/coupon.profile';
 
 @Module({
     imports: [RedisModule],
     controllers: [CouponController],
-    providers: [CouponService, CouponRepository],
+    providers: [CouponService, CouponRepository, CouponProfile],
     exports: [CouponService],
 })
 export class CouponModule { }
