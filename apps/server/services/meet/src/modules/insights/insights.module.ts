767e0b73c40194e4bf3fb102d432e19b8218a116
Lain <134944634+Lain4504@users.noreply.github.com>
2026-02-26 20:57:27 +0700

    refactor meet service

---
+++ apps/server/services/meet/src/modules/insights/insights.module.ts
@@ -0,0 +1,38 @@
+/**
+ * Insights Module
+ */
+
+import { Module, forwardRef } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { InsightsService } from './insights.service';
+import { RedisInsightsService } from '@server/meet/infrastructure/redis/redis-insights.service';
+import { NatsModule } from '@server/meet/services/nats.module';
+import { InsightsHandler } from '@server/meet/modules/insights/insights.handler';
+import { ArtifactsModule } from '@server/meet/modules/artifacts/artifacts.module';
+import { AnalyticsModule } from '@server/meet/modules/analytics/analytics.module';
+import { AppConfigService } from '@server/shared';
+
+@Module({
+    imports: [
+        NatsModule,
+        forwardRef(() => ArtifactsModule),
+        forwardRef(() => AnalyticsModule),
+        ClientsModule.registerAsync([
+            {
+                name: 'NATS_CLIENT',
+                imports: [],
+                useFactory: (appConfig: AppConfigService) => ({
+                    transport: Transport.NATS,
+                    options: {
+                        servers: [appConfig.nats.url],
+                    },
+                }),
+                inject: [AppConfigService],
+            },
+        ]),
+    ],
+    controllers: [InsightsHandler],
+    providers: [InsightsService, RedisInsightsService],
+    exports: [InsightsService, RedisInsightsService],
+})
+export class InsightsModule { }
